db4d8d509a6e3199a62cd2b4abe06b8a56070426
Rakishraj <74187235+Rakishraj@users.noreply.github.com>
2020-12-28 19:40:45 +0800

    Add files via upload

---
+++ BI19110076_LAB3_PART1_E2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stack>
+#include<string>
+
+using namespace std;
+
+bool ArePair(char opening,char closing)
+{
+	if(opening == '(' && closing == ')') return true;
+	else if(opening == '{' && closing == '}') return true;
+	else if(opening == '[' && closing == ']') return true;
+	return false;
+}
+
+bool AreParanthesesBalanced(string exp)
+{
+	stack<char>  S;
+	for(int i =0;i<exp.length();i++)
+	{
+		if(exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+			S.push(exp[i]);
+		else if(exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+		{
+			if(S.empty() || !ArePair(S.top(),exp[i]))
+				return false;
+			else
+				S.pop();
+		}
+	}
+	return S.empty() ? true:false;
+}
+
+int main()
+{
+	
+	string expression;
+	cout << "Example run:" << endl;
+	cout << "Input:  "; 
+	cin >> expression;
+	if(AreParanthesesBalanced(expression))
+		cout << "True\n";
+	else
+		cout << "False\n";
+}
